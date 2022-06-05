7e20eb7ca0439534ba1706420363d0c6b15ffe63
Simion Nacu <symyon.tech@gmail.com>
2022-06-04 20:31:49 -0400

    Deployed contract

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,19 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonic = fs.readFileSync("../.secret").toString().trim();
+const nodeEndPoint = fs.readFileSync("../.env").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, nodeEndPoint),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+    },
+  },
 };
