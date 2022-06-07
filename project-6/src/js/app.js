7c3e50fe10c5296a1f7e51f51142a2fe5f4eb707
Simion Nacu <symyon.tech@gmail.com>
2022-06-07 12:19:02 -0400

    removed SKU and all fileds for Distribuitor,Retailer and Consumer, all action are coming from the active account.

--- project-6/src/js/app.js
+++ project-6/src/js/app.js
@@ -2,20 +2,15 @@ App = {
   web3Provider: null,
   contracts: {},
   emptyAddress: "0x0000000000000000000000000000000000000000",
-  sku: 0,
   upc: 0,
   metamaskAccountID: "0x0000000000000000000000000000000000000000",
   ownerID: "0x0000000000000000000000000000000000000000",
-  originFarmerID: "0x0000000000000000000000000000000000000000",
   originFarmName: null,
   originFarmInformation: null,
   originFarmLatitude: null,
   originFarmLongitude: null,
   productNotes: null,
   productPrice: 0,
-  distributorID: "0x0000000000000000000000000000000000000000",
-  retailerID: "0x0000000000000000000000000000000000000000",
-  consumerID: "0x0000000000000000000000000000000000000000",
 
   init: async function () {
     App.readForm();
@@ -24,34 +19,24 @@ App = {
   },
 
   readForm: function () {
-    App.sku = $("#sku").val();
     App.upc = $("#upc").val();
     App.ownerID = $("#ownerID").val();
-    App.originFarmerID = $("#originFarmerID").val();
     App.originFarmName = $("#originFarmName").val();
     App.originFarmInformation = $("#originFarmInformation").val();
     App.originFarmLatitude = $("#originFarmLatitude").val();
     App.originFarmLongitude = $("#originFarmLongitude").val();
     App.productNotes = $("#productNotes").val();
     App.productPrice = $("#productPrice").val();
-    App.distributorID = $("#distributorID").val();
-    App.retailerID = $("#retailerID").val();
-    App.consumerID = $("#consumerID").val();
 
     console.log(
-      App.sku,
       App.upc,
       App.ownerID,
-      App.originFarmerID,
       App.originFarmName,
       App.originFarmInformation,
       App.originFarmLatitude,
       App.originFarmLongitude,
       App.productNotes,
-      App.productPrice,
-      App.distributorID,
-      App.retailerID,
-      App.consumerID
+      App.productPrice
     );
   },
 
@@ -207,14 +192,13 @@ App = {
 
   harvestItem: function (event) {
     event.preventDefault();
-    var processId = parseInt($(event.target).data("id"));
     App.readForm();
 
     App.contracts.SupplyChain.deployed()
       .then(function (instance) {
         return instance.harvestItem(
           App.upc,
-          App.originFarmerID,
+          App.metamaskAccountID,
           App.originFarmName,
           App.originFarmInformation,
           App.originFarmLatitude,
@@ -237,8 +221,6 @@ App = {
 
   processItem: function (event) {
     event.preventDefault();
-    var processId = parseInt($(event.target).data("id"));
-
     App.contracts.SupplyChain.deployed()
       .then(function (instance) {
         return instance.processItem(App.upc, { from: App.metamaskAccountID });
@@ -254,8 +236,6 @@ App = {
 
   packItem: function (event) {
     event.preventDefault();
-    var processId = parseInt($(event.target).data("id"));
-
     App.contracts.SupplyChain.deployed()
       .then(function (instance) {
         return instance.packItem(App.upc, { from: App.metamaskAccountID });
@@ -271,12 +251,9 @@ App = {
 
   sellItem: function (event) {
     event.preventDefault();
-    var processId = parseInt($(event.target).data("id"));
-
     App.contracts.SupplyChain.deployed()
       .then(function (instance) {
         const productPrice = web3.toWei(0.00001, "ether");
-        console.log("productPrice", productPrice);
         return instance.sellItem(App.upc, App.productPrice, {
           from: App.metamaskAccountID,
         });
@@ -292,8 +269,6 @@ App = {
 
   buyItem: function (event) {
     event.preventDefault();
-    var processId = parseInt($(event.target).data("id"));
-
     App.contracts.SupplyChain.deployed()
       .then(function (instance) {
         const walletValue = web3.toWei(3, "ether");
